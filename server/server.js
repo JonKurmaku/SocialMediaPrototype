e0527c6c51f3a29b86b05757c358c10a08efe861
JonKurmaku <129156284+JonKurmaku@users.noreply.github.com>
2024-06-19 14:09:54 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const clientPath = path.join(__dirname,'/../client');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(clientPath));
+
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
+})
