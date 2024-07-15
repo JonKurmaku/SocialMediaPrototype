54c6d3c5653eed09e2cfb4e4c1d43a6c38644139
JonKurmaku <jonikurmaku@gmail.com>
2024-07-15 22:10:51 +0200

    commit 2

--- server/server.js
+++ server/server.js
@@ -1,26 +1,35 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const chats = require("./data/data.js");
-const connectDB = require("./config/db.js");
-const userRoutes = require("./routes/userRoutes.js");
+const express = require("express")
 
-var app = express();
-dotenv.config();
+const dotenv = require("dotenv")
+const chats = require("./data/data.js")
+const connectDB = require("./config/db.js")
+
+const {notFound,errorHandler} = require("./middleware/errorMiddleware.js")
+
+const chatRoutes = require("./routes/chatRoutes.js")
+const userRoutes = require("./routes/userRoutes.js")
+
+var app = express()
+dotenv.config()
 connectDB()
 
 
-var port = process.env.PORT || 3000;
-console.log(process.env.PORT);
+var port = process.env.PORT || 3000
+console.log(process.env.PORT)
 console.log(port);
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send("HELLO XHAXHI");
+    res.send("HELLO XHAXHI")
 })
 
 app.use('/api/user',userRoutes)
+app.use('/api.chat',chatRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port,()=>{
-    console.log(`Server is up on port ${port}`);
+    console.log(`Server is up on port ${port}`)
 })
