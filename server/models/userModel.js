73648dea0936b9f0513c003b339acee4cda263ff
ArionDev88 <128810425+ArionDev8@users.noreply.github.com>
2024-07-27 10:43:13 +0200

    Small Change

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userModel = mongoose.Schema({
+const userModel = mongoose.Schema({  
     name: {type:String, required:true},
     email: {type:String, required:true, unique:true},
     password: {type:String, required:true},
